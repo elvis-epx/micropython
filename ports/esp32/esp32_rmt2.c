e0bf8d5a0e1f4ea26faafd4509c5157e474a51c9
Elvis Pfutzenreuter <epxx@epxx.co>
2024-11-19 19:30:37 -0300

    Callback capabiity

--- ports/esp32/esp32_rmt2.c
+++ ports/esp32/esp32_rmt2.c
@@ -15,11 +15,13 @@ extern const mp_obj_type_t esp32_rmt2_type;
 typedef struct _esp32_rmt2_obj_t {
     mp_obj_base_t base;
     gpio_num_t pin;
+    mp_obj_t cb;
     bool continue_rx;
 } esp32_rmt2_obj_t;
 
 static rmt_channel_handle_t rx_channel = NULL;
 static rmt_symbol_word_t symbols[64];
+
 static rmt_rx_done_event_data_t rx_data;
 
 static rmt_receive_config_t rx_config = {
@@ -34,11 +36,19 @@ static rmt_rx_channel_config_t rx_ch_conf = {
     .mem_block_symbols = 64,
 };
 
-static mp_obj_t rmt_recv_done_upperhalf(mp_obj_t _) {
-    // FIXME handle
+static mp_obj_t rmt_recv_done_upperhalf(mp_obj_t self_in) {
+    esp32_rmt2_obj_t *self = MP_OBJ_TO_PTR(self_in);
+
+    if (self->cb == mp_const_none) {
+        return mp_const_none;
+    }
+
     size_t len = rx_data.num_symbols;
-    mp_printf(MP_PYTHON_PRINTER, "RMT2 tot=%u ", len);
     rmt_symbol_word_t *data = rx_data.received_symbols;
+    int odd = (data[len - 1].duration1 == 0) ? 1 : 0;
+    mp_obj_t list = mp_obj_new_list(len * 2 - odd, NULL);
+    mp_obj_list_t *list_in = MP_OBJ_TO_PTR(list);
+
     for (uint8_t i = 0; i < len; i++) {
         int n0 = (uint16_t) data[i].duration0;
         int n1 = (uint16_t) data[i].duration1;
@@ -48,9 +58,14 @@ static mp_obj_t rmt_recv_done_upperhalf(mp_obj_t _) {
         if (!data[i].level1) {
             n1 = -n1;
         }
-        mp_printf(MP_PYTHON_PRINTER, "%d %d ", n0, n1);
+        list_in->items[i * 2 + 0] = mp_obj_new_int(n0);
+        if ((i < (len-1)) || (!odd)) {
+            list_in->items[i * 2 + 1] = mp_obj_new_int(n1);
+        }
     }
-    mp_printf(MP_PYTHON_PRINTER, "\n");
+
+    mp_sched_schedule(self->cb, list);
+
     return mp_const_none;
 }
 
@@ -59,7 +74,8 @@ static MP_DEFINE_CONST_FUN_OBJ_1(rmt_recv_done_upperhalf_obj, rmt_recv_done_uppe
 static bool IRAM_ATTR rmt_recv_done(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata, void *udata){
     rx_data = *edata;
     esp32_rmt2_obj_t *self = udata;
-    mp_sched_schedule(MP_OBJ_FROM_PTR(&rmt_recv_done_upperhalf_obj), mp_const_none);
+    // FIXME copy rx data to temporary buffer to avoid being clobbered
+    mp_sched_schedule(MP_OBJ_FROM_PTR(&rmt_recv_done_upperhalf_obj), MP_OBJ_FROM_PTR(self));
     if (self->continue_rx) {
         rmt_receive(rx_channel, symbols, sizeof(symbols), &rx_config);
     }
@@ -81,13 +97,19 @@ static esp_err_t rmt_do_install(void *self)
 static mp_obj_t esp32_rmt2_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
     static const mp_arg_t allowed_args[] = {
         { MP_QSTR_pin,       MP_ARG_REQUIRED | MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
+        { MP_QSTR_cb,        MP_ARG_REQUIRED | MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
     };
     mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all_kw_array(n_args, n_kw, all_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
     gpio_num_t pin_id = machine_pin_get_id(args[0].u_obj);
+    mp_obj_t cb = args[1].u_obj;
+    if (cb != mp_const_none && !mp_obj_is_callable(cb)) {
+        mp_raise_ValueError(MP_ERROR_TEXT("cb is not callable"));
+    }
 
     esp32_rmt2_obj_t *self = mp_obj_malloc_with_finaliser(esp32_rmt2_obj_t, &esp32_rmt2_type);
     rx_ch_conf.gpio_num = self->pin = pin_id;
+    self->cb = cb;
 
     check_esp_err(rmt_do_install(self));
     rx_data.num_symbols = 0;
@@ -110,6 +132,8 @@ static mp_obj_t esp32_rmt2_deinit(mp_obj_t self_in) {
         rmt_del_channel(rx_channel);
         self->pin = -1;
     }
+    self->cb = mp_const_none;
+
     return mp_const_none;
 }
 static MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt2_deinit_obj, esp32_rmt2_deinit);

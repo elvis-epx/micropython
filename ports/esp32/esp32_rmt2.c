c41edc4fd9be1ee4c09da70398e0ab396a73a5cd
Elvis Pfutzenreuter <epxx@epxx.co>
2024-11-20 10:25:15 -0300

    Configurable buf size

--- ports/esp32/esp32_rmt2.c
+++ ports/esp32/esp32_rmt2.c
@@ -19,7 +19,7 @@ def cb(l):
     totalb += len(l)
     print(len(l), l)
 
-r = esp32.RMT2(pin=p, cb=cb)
+r = esp32.RMT2(pin=p, cb=cb, buf=128)
 r.read_pulses()
 */
 
@@ -30,9 +30,6 @@ void machine_bitstream_high_low(mp_hal_pin_obj_t pin, uint32_t *timing_ns, const
 // Forward declaration
 extern const mp_obj_type_t esp32_rmt2_type;
 
-// FIXME make it configurable
-#define SYMBOL_BUF_LEN 64
-
 // Python object impl structure
 typedef struct _esp32_rmt2_obj_t {
     mp_obj_base_t base;
@@ -40,7 +37,9 @@ typedef struct _esp32_rmt2_obj_t {
     mp_obj_t cb;
     bool continue_rx;
     rmt_channel_handle_t rx_channel;
-    rmt_symbol_word_t symbols[SYMBOL_BUF_LEN];
+    rmt_symbol_word_t *symbols;
+    size_t symbols_size;
+    size_t num_symbols;
     // rmt_rx_done_event_data_t rx_data;
     rmt_receive_config_t rx_config;
     rmt_rx_channel_config_t rx_ch_conf;
@@ -48,7 +47,7 @@ typedef struct _esp32_rmt2_obj_t {
     // double buffering of received data
     bool recv_locked;
     size_t recv_num_symbols;
-    rmt_symbol_word_t recv_symbols[SYMBOL_BUF_LEN];
+    rmt_symbol_word_t *recv_symbols;
 } esp32_rmt2_obj_t;
 
 // Non-public function called by interrupt handler to finish data processing
@@ -99,14 +98,14 @@ static bool IRAM_ATTR rmt_recv_done(rmt_channel_handle_t channel, const rmt_rx_d
 
     if (!self->recv_locked) {
             self->recv_num_symbols = edata->num_symbols;
-            memcpy(self->recv_symbols, edata->received_symbols, SYMBOL_BUF_LEN * sizeof(rmt_symbol_word_t));
+            memcpy(self->recv_symbols, edata->received_symbols, edata->num_symbols * sizeof(rmt_symbol_word_t));
             self->recv_locked = true;
     }
 
     mp_sched_schedule(MP_OBJ_FROM_PTR(&rmt_recv_done_upperhalf_obj), MP_OBJ_FROM_PTR(self));
 
     if (self->continue_rx) {
-        rmt_receive(self->rx_channel, self->symbols, sizeof(self->symbols), &self->rx_config);
+        rmt_receive(self->rx_channel, self->symbols, self->symbols_size, &self->rx_config);
     }
 
     return false;
@@ -115,20 +114,36 @@ static bool IRAM_ATTR rmt_recv_done(rmt_channel_handle_t channel, const rmt_rx_d
 static mp_obj_t esp32_rmt2_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
     static const mp_arg_t allowed_args[] = {
         { MP_QSTR_pin,       MP_ARG_REQUIRED | MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
-        { MP_QSTR_cb,        MP_ARG_REQUIRED | MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
+        { MP_QSTR_cb,        MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
+        { MP_QSTR_buf,       MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = 64} },
     };
     mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all_kw_array(n_args, n_kw, all_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
     gpio_num_t pin_id = machine_pin_get_id(args[0].u_obj);
     mp_obj_t cb = args[1].u_obj;
     if (cb != mp_const_none && !mp_obj_is_callable(cb)) {
         mp_raise_ValueError(MP_ERROR_TEXT("cb is not callable"));
+    }
+    int num_symbols = args[2].u_int;
+    if ((num_symbols < 64) || ((num_symbols % 2) == 1)) {
+        mp_raise_ValueError(MP_ERROR_TEXT("buf must be at least 64 and even"));
     }
 
     esp32_rmt2_obj_t *self = mp_obj_malloc_with_finaliser(esp32_rmt2_obj_t, &esp32_rmt2_type);
 
     self->cb = cb;
     self->rx_channel = NULL;
+    self->num_symbols = num_symbols;
+    self->symbols_size = num_symbols * sizeof(rmt_symbol_word_t);
+    self->symbols = m_realloc(NULL, self->symbols_size);
+    self->recv_symbols = m_realloc(NULL, self->symbols_size);
+
+    // RMT new driver does not have a way to specify a group clock divisor, see
+    // https://github.com/espressif/esp-idf/issues/14760#issuecomment-2430770601 and
+    // https://github.com/espressif/esp-idf/issues/11262
+    // since signal_range_min_ns must fit in an 8-bit register, the value is limited
+    // to 3190ns (80MHz clock * 3190 / 1000000000 [conversion to ns] == 255).
 
     // FIXME configurable
     self->rx_config.signal_range_min_ns = 3100;
@@ -139,8 +154,7 @@ static mp_obj_t esp32_rmt2_make_new(const mp_obj_type_t *type, size_t n_args, si
     self->rx_ch_conf.clk_src = RMT_CLK_SRC_DEFAULT;
     // FIXME configurable
     self->rx_ch_conf.resolution_hz = 1000000;
-    // FIXME configurable
-    self->rx_ch_conf.mem_block_symbols = SYMBOL_BUF_LEN;
+    self->rx_ch_conf.mem_block_symbols = num_symbols;
 
     check_esp_err(rmt_new_rx_channel(&self->rx_ch_conf, &self->rx_channel));
     rmt_rx_event_callbacks_t cbs = {
@@ -157,7 +171,9 @@ static mp_obj_t esp32_rmt2_make_new(const mp_obj_type_t *type, size_t n_args, si
 
 static void esp32_rmt2_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
     esp32_rmt2_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    mp_printf(print, "RMT2 %u %s", self->recv_num_symbols, self->recv_locked ? "locked" : "unlocked");
+    mp_printf(print, "RMT2 pin=%u buf=%u(x2) %u %s",
+                    self->pin, self->num_symbols,
+                    self->recv_num_symbols, self->recv_locked ? "locked" : "unlocked");
 }
 
 
@@ -170,6 +186,10 @@ static mp_obj_t esp32_rmt2_deinit(mp_obj_t self_in) {
         self->pin = -1;
     }
     self->cb = mp_const_none;
+    m_free(self->symbols);
+    self->symbols = 0;
+    m_free(self->recv_symbols);
+    self->recv_symbols = 0;
 
     return mp_const_none;
 }
@@ -192,7 +212,7 @@ static mp_obj_t esp32_rmt2_read_pulses(mp_obj_t self_in) {
     self->continue_rx = true;
     self->recv_locked = false;
     self->recv_num_symbols = 0;
-    check_esp_err(rmt_receive(self->rx_channel, self->symbols, sizeof(self->symbols), &self->rx_config));
+    check_esp_err(rmt_receive(self->rx_channel, self->symbols, self->symbols_size, &self->rx_config));
     return mp_const_none;
 }
 static MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt2_read_pulses_obj, esp32_rmt2_read_pulses);
